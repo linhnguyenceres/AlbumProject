0f71b16a494f9b0ae5fa70f95684911adea5719e
linhnn37 <linhnn37@fpt.com.vn>
2021-12-14 23:32:29 +0700

    update setting UI

--- app/src/main/java/com/example/albumproject/activities/SettingActivity.java
+++ app/src/main/java/com/example/albumproject/activities/SettingActivity.java
@@ -1,20 +1,45 @@
 package com.example.albumproject.activities;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.albumproject.R;
 
-public class SettingActivity extends AppCompatActivity {
+public class SettingActivity extends Activity {
+    View btnBack;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.setting);
-        ActionBar action1 = getSupportActionBar();
-        action1.setTitle("Cài đặt");
-        action1.setDisplayHomeAsUpEnabled(true);
+        setContentView(R.layout.activity_setting);
+//        ActionBar action1 = getSupportActionBar();
+//        action1.setTitle(getString(R.string.setting));
+//        action1.setDisplayHomeAsUpEnabled(true);
+        getViews();
+        initClick();
+    }
 
+    void getViews() {
+        btnBack = (View) findViewById(R.id.btnBack);
+    }
+
+    void initClick() {
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        // code here to show dialog
+        super.onBackPressed();  // optional depending on your needs
     }
 }
